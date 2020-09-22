6c482ecff60c7f54e40920dc25c506e54fa7b65d
Jacob Felknor <jacobfelknor073@gmail.com>
2020-09-21 23:11:01 -0600

    rm pycache. ACtually log to db

--- speedtestlog/management/commands/runspeedtest.py
+++ speedtestlog/management/commands/runspeedtest.py
@@ -1,14 +1,25 @@
 from django.core.management.base import BaseCommand
 import speedtest
+from speedtestlog.models import SpeedTestResult
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        print("running speedtest...")
+        print("Running speedtest...")
         st = speedtest.Speedtest()
         st.download()
         st.upload()
         st.get_best_server()
         results = st.results.dict()
 
-        print(results)
+        SpeedTestResult.objects.create(
+            **{
+                "timestamp": results["timestamp"],
+                "ping": results["ping"],
+                "upload": results["upload"],
+                "download": results["download"],
+                "json": results,
+            }
+        )
+
+        print("Done!")
--- speedtestlog/models.py
+++ speedtestlog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SpeedTestResult(models.Model):
+    timestamp = models.DateTimeField()
+    ping = models.FloatField(null=True)
+    download = models.FloatField(null=True)
+    upload = models.FloatField(null=True)
+    json = models.JSONField()
