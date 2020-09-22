a40832f545cf2983a6fd6b1b3ee75b52fd609f9c
Jacob Felknor <jacobfelknor073@gmail.com>
2020-09-22 09:06:39 -0600

    oops. Now I added it

--- speedtestlog/management/commands/runspeedtest.py
+++ speedtestlog/management/commands/runspeedtest.py
@@ -6,12 +6,17 @@ from speedtestlog.models import SpeedTestResult
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         print("Running speedtest...")
+        # speedtest results are MUCH lower on first run for some reason
+        # run a "warm up" run, then use the results from the second run
         st = speedtest.Speedtest()
         st.download()
         st.upload()
         st.get_best_server()
+        # Run the second, more accurate run
+        st.download()
+        st.upload()
+        st.get_best_server()
         results = st.results.dict()
-
         SpeedTestResult.objects.create(
             **{
                 "timestamp": results["timestamp"],
